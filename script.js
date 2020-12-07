02a47c0a66017f1521b3e01192d04a0501bad1e2
Sheshu <reddysheshasena@gmail.com>
2020-12-08 01:46:27 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let buttonColours=["red", "blue", "green", "yellow"];
+let gamePattern=[];
+function nextSequence()
+{
+    let randomNumber= Math.floor(Math.random()*4);
+    let randomChosenColour=buttonColours[randomNumber]; 
+    gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    let audio=new audio("/patternMemory/sounds/"+randomChosenColour+".mp3");
+    audio.play();
+}
+
+
